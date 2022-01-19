c1b431aebdcd0934c98a5a7917f831b819b3f696
EvadeGame <EvadeGame@outlook.com>
2022-01-19 15:23:25 +0800

    add start with windows function, update icons

--- Program.cs
+++ Program.cs
@@ -2,15 +2,12 @@
 using System.Windows.Forms;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace VirtualDesktopSwitcher
 {
     static class Program
     {
-        [STAThread]
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-
         public const int KEYEVENTF_KEYDOWN = 0x0000; //Key down flag
         public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
 
@@ -23,101 +20,98 @@ namespace VirtualDesktopSwitcher
         public const int VK_F4 = 0x0073;
         public const int VK_TAB = 0x0009;
 
-        static NotifyIcon notifyIconLeft = new NotifyIcon();
-        static NotifyIcon notifyIconRight = new NotifyIcon();
+        private static NotifyIcon notifyIconLeft;
+        private static NotifyIcon notifyIconRight;
 
+        private static bool StartWithWindows;
+
+        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Initialize Left Notify Icon
-            notifyIconLeft.Text="Left";
-            notifyIconLeft.ContextMenuStrip = GetContext();
-            notifyIconLeft.Icon = new Icon("Left.ico");
+            StartWithWindows = IsStartWithWindows();
+            var contextMenuStrip = GetContext();
 
-            //Initialize Right Notify Icon
-            notifyIconRight.Text = "Right";
-            notifyIconRight.ContextMenuStrip = GetContext();
-            notifyIconRight.Icon = new Icon("Right.ico");
+            notifyIconLeft = new NotifyIcon() {
+                Text = "Left",
+                ContextMenuStrip = contextMenuStrip,
+                Icon = Properties.Resources.left_light,
+                Visible = true,
+            };
 
-            notifyIconLeft.Visible = true;
-            notifyIconRight.Visible = true;
+            notifyIconRight = new NotifyIcon {
+                Text = "Right",
+                ContextMenuStrip = contextMenuStrip,
+                Icon = Properties.Resources.right_light,
+                Visible = true,
+            };
 
-            notifyIconLeft.MouseClick += LeftClickHandler;
-            notifyIconRight.MouseClick += RightClickHandler;
+            notifyIconLeft.MouseClick += (o, e) => {
+                if (e.Button == MouseButtons.Left)
+                {
+                    GoToLeftDesktop();
+                }
+                else if (e.Button== MouseButtons.Right)
+                {
+                    StartWithWindows = IsStartWithWindows();
+                    (contextMenuStrip.Items[3] as ToolStripMenuItem).Checked = StartWithWindows;
+                }
+            };
+
+            notifyIconRight.MouseClick += (o, e) => {
+                if (e.Button == MouseButtons.Left)
+                {
+                    GoToRightDesktop();
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    StartWithWindows = IsStartWithWindows();
+                    (contextMenuStrip.Items[3] as ToolStripMenuItem).Checked = StartWithWindows;
+                }
+            };
 
             Application.Run();
         }
 
-        //Contex Menu
         private static ContextMenuStrip GetContext()
         {
             ContextMenuStrip CMS = new ContextMenuStrip();
-            CMS.Items.Add("About", null, new EventHandler(About_Program));
-            CMS.Items.Add("Close Current Desktop", null, new EventHandler(Close_Current_Desktop));
-            CMS.Items.Add("Open New Desktop", null, new EventHandler(Open_New_Desktop));
-            CMS.Items.Add("Exit",null,new EventHandler(Exit_Click));
+
+            CMS.Items.Add("About", null, new EventHandler((o, e) => System.Diagnostics.Process.Start("https://github.com/hangacs/VirtualDesktopSwitcher")));
+            CMS.Items.Add("Close Current Desktop", null, new EventHandler((o, e) => CloseCurrentDesktop()));
+            CMS.Items.Add("Open New Desktop", null, new EventHandler((o, e) => OpenNewDesktop()));
+            CMS.Items.Add(new ToolStripMenuItem("StartWithWindows", null,
+                (o, e) => {
+                    StartWithWindows = !StartWithWindows;
+                    AutoStart(StartWithWindows);
+                    (o as ToolStripMenuItem).Checked = StartWithWindows;
+                }) {
+                Checked = StartWithWindows
+            });
+            CMS.Items.Add("Exit", null, new EventHandler((o, e) => {
+                notifyIconLeft.Dispose();
+                notifyIconRight.Dispose();
+                Application.Exit();
+            }));
 
             return CMS;
         }
-        private static void About_Program(object sender,EventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/hangacs/VirtualDesktopSwitcher");
-        }
-
-        private static void Close_Current_Desktop(object sender, EventArgs e)
-        {
-            CloseCurentDesktop();
-        }
-        private static void Open_New_Desktop(object sender, EventArgs e)
-        {
-            OpenNewDesktop();
-        }
-
-        private static void Exit_Click(object sender, EventArgs e)
-        {
-            notifyIconLeft.Dispose();
-            notifyIconRight.Dispose();
-            Application.Exit();
-        }
-
-        //Switch Virtual Desktop
-        private static void LeftClickHandler(object sender,MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                GoLeftDesktop();
-            }
-        }
-        private static void RightClickHandler(object sender,MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                GoRightDesktop();
-            }
-        }
 
         #region Keyboard Event
-        public static void OpenNewDesktop()
-        {
-            CombineKey(KEY_D);
-        }
-        public static void CloseCurentDesktop()
-        {
-            CombineKey(VK_F4);
-        }
-        public static void GoLeftDesktop()
-        {
-            CombineKey(VK_LEFT);
-        }
-        public static void GoRightDesktop()
-        {
-            CombineKey(VK_RIGHT);
-        }
-        #endregion
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
 
-        public static void CombineKey(byte KEY)
+        public static void OpenNewDesktop() => CombineKey(KEY_D);
+
+        public static void CloseCurrentDesktop() => CombineKey(VK_F4);
+
+        public static void GoToLeftDesktop() => CombineKey(VK_LEFT);
+
+        public static void GoToRightDesktop() => CombineKey(VK_RIGHT);
+
+        private static void CombineKey(byte KEY)
         {
             keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYDOWN, 0);
             keybd_event(VK_WIN, 0, KEYEVENTF_KEYDOWN, 0);
@@ -126,6 +120,56 @@ namespace VirtualDesktopSwitcher
             keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
             keybd_event(VK_WIN, 0, KEYEVENTF_KEYUP, 0);
             keybd_event(KEY, 0, KEYEVENTF_KEYUP, 0);
+        }
+        #endregion Keyboard Event
+
+        private static bool IsStartWithWindows()
+        {
+            try
+            {
+                bool result;
+                RegistryKey R_local = Registry.CurrentUser;//RegistryKey R_local = Registry.CurrentUser;
+                RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                result = Application.ExecutablePath == R_run.GetValue("VirtualDesktopSwitcher") as string;
+                R_run.Close();
+                R_local.Close();
+
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to add registry key. {ex.Message}", "Error");
+                return false;
+            }
+        }
+
+        private static void AutoStart(bool autoStart)
+        {
+            try
+            {
+                if (autoStart)
+                {
+                    RegistryKey R_local = Registry.CurrentUser;//RegistryKey R_local = Registry.CurrentUser;
+                    RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                    R_run.SetValue("VirtualDesktopSwitcher", Application.ExecutablePath);
+                    R_run.Close();
+                    R_local.Close();
+                }
+                else
+                {
+                    RegistryKey R_local = Registry.CurrentUser;//RegistryKey R_local = Registry.CurrentUser;
+                    RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                    R_run.DeleteValue("VirtualDesktopSwitcher", false);
+                    R_run.Close();
+                    R_local.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to add/remove registry key. {ex.Message}", "Error");
+            }
         }
     }
 }
